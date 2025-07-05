c7ed8e6c3ed9b21d49adb89d5c00cf09304aa11c
laurenz-coac <laurenz.schindler@coac.de>
2025-07-06 01:29:53 +0200

    increase batch size

--- unetr_hyperparameter_search.py
+++ unetr_hyperparameter_search.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     print(f"Device: {device}", flush=True)
 
     DATA_ROOT = "/scratch/cv-course2025/lschind5/kits19/data"
-    BATCH_SIZE = 8
+    BATCH_SIZE = 16
     NUM_EPOCHS = 25
     NUM_WORKERS = 4
 
